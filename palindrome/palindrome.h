ee79d5b1f93922a00520152b94fa75a7a8dd90bc
pworontzoff <pierre.worontzoff@hepl.be>
2025-12-01 11:41:14 +0100

    Update palindrome.h

--- palindrome/palindrome.h
+++ palindrome/palindrome.h
@@ -1,4 +1,4 @@
-int palindrome(char chaine[]) {
+int palindrome(char *chaine) {
 	// Ecrire le code de la fonction palindrome
 	// la fonction renvoie 1 si le mot est un palindrome et 0 sinon
 	return 0;
